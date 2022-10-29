3430a5a168cef0a27bdc1a307afbfdbe95faf7ec
kaligurl2008 <kaligurl2008@gmail.com>
2022-10-29 12:55:41 -0400

    built up navbar and route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Product from './components/Product/Product';
 import Homepage from './components/Homepage/Homepage';
 import Cart from './components/Cart/Cart';
 import ProductDetail from './components/ProductDetail/ProductDetail';
+import Navbar from './components/Navbar/Navbar';
 
 
 function App() {
@@ -39,12 +40,13 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route exact path = '/' element={<Homepage />} />
         <Route path = '/cart' element={<Cart />} />
         <Route path = '/detail' element={<ProductDetail />} />
-
       </Routes>
+      {/*footer goes here*/}
       </BrowserRouter>
     
       
